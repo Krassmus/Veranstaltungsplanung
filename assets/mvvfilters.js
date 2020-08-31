cf6e3a1431bf325f5f46a604e2c4e915e63f46e1
Rasmus Fuhse <fuhse@data-quest.de>
2020-08-31 10:30:05 +0200

    remove debugging

--- assets/mvvfilters.js
+++ assets/mvvfilters.js
@@ -12,7 +12,6 @@ jQuery(function () {
     });
     jQuery(document).on("change", ".sidebar-widget.courses select[name=stgteil_id]", function () {
         let stgteil_id = jQuery(this).val();
-        console.log(stgteil_id);
         window.setTimeout(function () {
             $(".sidebar-widget.courses.modulteilfilter .sidebar-widget-content form select").load(
                 STUDIP.URLHelper.getURL("plugins.php/veranstaltungsplanung/mvvfilters/get_mvv_modulteil") + " form select option",
